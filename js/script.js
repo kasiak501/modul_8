8b97ef19f5ffe3694f62e2fea2a521601478b2b3
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-17 23:59:23 +0200

    Create triangle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // scripts.js
 
-var name = prompt('Enter your name'); //var string
+//var name = prompt('Enter your name'); 
 //alert('Hello, ' + name);
-console.log('Hello, ' + name); /*The console.log() method writes a message to the console*/
+//console.log('Hello, ' + name); /*The console.log() method writes a message to the console*/
+
+//triangle area
+
+var a= 10,
+    h=60,
+    trianglearea=prompt("Do you agree that triangle area is?:",a*h/2);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
