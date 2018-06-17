32a4be1e8e8805a0f3dcff5b09fd85434c6270ae
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-17 23:11:14 +0200

    Add comments to js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var name = prompt('Enter your name'); //var string
+//alert('Hello, ' + name);
+console.log('Hello, ' + name); /*The console.log() method writes a message to the console*/
