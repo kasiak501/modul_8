1068a59c637cfd6bef7d51d6cfaa33c705e129c6
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-20 14:02:11 +0200

    Add zadanie 8.6

--- js/script.js
+++ js/script.js
@@ -6,11 +6,21 @@ alert('Hello, ' + name);/*
 
 //triangle area
 
-var a = 10,
+/*var a = 10,
     h = 60,
     triangleArea = prompt("Do you agree that triangle area is?:", a * h / 2);
-    
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);*/
 
 
+var a = 6,
+    b = 10,
+    Value = (a * a) - (2 * a * b) - (b * b);
 
+if (Value > 0) {
+    consol.log('Wynik dodatni');
+} else if (Value < 0) {
+    consol.log('Wynik ujemny');
+} else {
+    consol.log('Wynik równy zero');
+}
